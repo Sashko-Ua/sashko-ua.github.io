e88b1bb6e5887a418cd550f51cca6e438d97ee8d
Sashko Shablenskyi <56129170+sashko-ua-94@users.noreply.github.com>
2020-03-18 18:10:58 +0200

    add skroll

--- js/script.js
+++ js/script.js
@@ -42,9 +42,12 @@ $(document).ready(function(){
 
 	toggleSlide('.next')
     toggleSlide('.back')
- });
 
- 
-
-
- 
+    $('a[href^="#"]').bind("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+         scrollTop: $(anchor.attr("href")).offset().top
+        }, 1000);
+        e.preventDefault();
+    });
+});
