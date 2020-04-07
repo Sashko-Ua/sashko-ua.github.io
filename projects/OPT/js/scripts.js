785c5c716f1e5f4d89bd056536998ee4f4a44ec8
Sashko Shablenskyi <56129170+sashko-ua-94@users.noreply.github.com>
2020-04-07 21:52:20 +0300

    add work

---
+++ projects/OPT/js/scripts.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+
+	// _________SLICK_________
+	$('.customers__carousel').slick({
+		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/arrow_left.png" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/arrow_right.png" alt=""></button>',
+		// autoplay: true,
+		autoplaySpeed: 3000,
+		speed: 800,
+		variableWidth: true,
+		centerMode: true,
+		slidesToShow: 3,
+		responsive: [
+			{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				slidesToShow: 3,
+				dots: true
+			}
+			},
+			{
+			breakpoint: 480,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				slidesToShow: 1,
+				dots: true
+			}
+			}
+		]
+	});
+
+	// __________HAMBURGER__________
+	window.addEventListener('DOMContentLoaded', () => {
+		const menu = document.querySelector('.mini'),
+		menuItem = document.querySelectorAll('.menu_item'),
+		hamburger = document.querySelector('.hamburger');
+	
+		hamburger.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger_active');
+			menu.classList.toggle('mini_active');
+		});
+	
+		menuItem.forEach(item => {
+			item.addEventListener('click', () => {
+				hamburger.classList.toggle('hamburger_active');
+				menu.classList.toggle('menu_active');
+			})
+		})
+	})
+
+	// __________Scroll__________
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.arrow').fadeIn();
+        } else {
+            $('.arrow').fadeOut();
+        }
+    });
+
+    $('a[href^="#"]').bind("click", function(e){
+        const anchor = $(this);
+        $('html, body').stop().animate({
+         scrollTop: $(anchor.attr("href")).offset().top
+        }, 1000);
+        e.preventDefault();
+    });
+});
+	
