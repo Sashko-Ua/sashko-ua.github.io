45b38bc8bf03b6e40c10a557a68aa65bea1ced26
Sashko <sashkoace@gmail.com>
2020-09-04 20:28:23 +0300

    small update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
 "use strict";
 
-$(document).ready(function () {
+window.addEventListener('DOMContentLoaded', () => {
+    // __________SCROLLING ARROW__________
+    const arrowUP = document.querySelector('.arrow-up');
+
+    arrowUP.addEventListener('click', () => {
+        window.scrollTo(pageYOffset, 1500);
+    })
+
+    window.addEventListener('scroll', () => {
+        arrowUP.hidden = (pageYOffset < document.documentElement.clientHeight);
+    });
+
+    // __________SMOOTH SCROLLING__________
+    const anchors = document.querySelectorAll('a.scroll-to')
+
+    SmoothScrolling(anchors);
+
+    function SmoothScrolling(element) {
+        for (let anchor of element) {
+            anchor.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                const blockID = anchor.getAttribute('href')
+
+                document.querySelector(blockID).scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            })
+        }
+    }
+
     // __________HAMBURGER__________
     const menu = document.querySelector('.menu'),
         menuItem = document.querySelectorAll('.menu__item'),
@@ -31,56 +62,29 @@ $(document).ready(function () {
         loop: true
     });
 
-    // __________POPUP__________
-    $('.image-link').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true,
-        },
-        zoom: {
-            enabled: true,
-            duration: 400,
-            easing: 'ease-in-out',
-        },
-        closeBtnInside: false,
-        closeOnBgClick: false
+    // // __________POPUP__________
+    // $('.image-link').magnificPopup({
+    //     type: 'image',
+    //     gallery: {
+    //         enabled: true,
+    //     },
+    //     zoom: {
+    //         enabled: true,
+    //         duration: 400,
+    //         easing: 'ease-in-out',
+    //     },
+    //     closeBtnInside: false,
+    //     closeOnBgClick: false
+    // });
+
+    // __________Tab__________
+    const btn = document.querySelector('.tab__indicators'),
+        item = document.querySelectorAll('.tab__item'),
+        indicators = document.querySelectorAll('.tab__indicator');
+
+    btn.addEventListener('click', () => {
+        item.forEach(e => e.classList.toggle('tab__item_active'));
+
+        indicators.forEach(e => e.classList.toggle('tab__indicator_active'));
     });
-
-    // __________SCROLLING ARROW__________
-    const arrowUP = document.getElementById('arrow-up');
-
-    arrowUP.onclick = function () {
-        window.scrollTo(pageXOffset, 1500);
-    };
-
-    window.addEventListener('scroll', function () {
-        arrowUP.hidden = (pageYOffset < document.documentElement.clientHeight);
-    });
-
-    // __________SMOOTH SCROLLING__________
-    const anchors = document.querySelectorAll('a.scroll-to')
-
-    for (let anchor of anchors) {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault()
-
-            const blockID = anchor.getAttribute('href')
-
-            document.querySelector(blockID).scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            })
-        })
-    }
-
-    // __________WOW__________
-    const wow = new WOW(
-        {
-            boxClass: 'wow',
-            animateClass: 'animated',
-            offset: 300,
-            mobile: true,
-            live: true
-        });
-    wow.init();
 });
